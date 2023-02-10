72c52a8368177f1fee19a8adf2072b7b43167037
unknown <git@github.com:VicThorMetaNode>
2023-02-10 18:32:11 +0100

    full backend done + mongoose connexion

--- server/schemas/schema.js
+++ server/schemas/schema.js
@@ -6,11 +6,11 @@ const Client = require('../models/Client')
 const Project = require('../models/Project')
 
 
-//GRAPHQL SETUP
+//-------------------- GRAPHQL SETUP
 //WHEN HAVING NUMEROUS DATA AS PROJECTS & CLIENTS YOU NEED TYPE AS REF
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList} = require('graphql');
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList, GraphQLNonNull, GraphQLEnumType} = require('graphql');
 
-//Client Type: convention use Capitalize 
+//CLIENT TYPE: convention use Capitalize 
 const ClientType = new GraphQLObjectType({
     name: 'Client',
     fields: () => ({
@@ -21,7 +21,7 @@ const ClientType = new GraphQLObjectType({
     })
 });
 
-//Project Type: convention use Capitalize 
+//PROJECT TYPE: convention use Capitalize 
 const ProjectType = new GraphQLObjectType({
     name: 'Project',
     fields: () => ({
@@ -87,7 +87,143 @@ const RootQuery = new GraphQLObjectType({
             }
     }
 });
+
+//--------------- MUTATIONS
+//everything is an object type
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+      //ADD CLIENT
+      addClient: {
+        type: ClientType,
+        args: {
+            //if you want input as mandatory use GraphQLNonNull
+          name: { type: GraphQLNonNull(GraphQLString) },
+          email: { type: GraphQLNonNull(GraphQLString) },
+          phone: { type: GraphQLNonNull(GraphQLString) },
+        },
+        resolve(parent, args) {
+            const client = new Client({
+              name: args.name,
+              email: args.email,
+              phone: args.phone,
+            });
+    
+            return client.save();
+          },
+        },
+        //DELETE CLIENT
+        deleteClient: {
+            type: ClientType,
+            args: {
+              id: { type: GraphQLNonNull(GraphQLID) },
+            },
+            resolve(parent, args) {
+                return Client.findByIdAndRemove(args.id);
+            //   Project.find({ clientId: args.id }).then((projects) => {
+            //     projects.forEach((project) => {
+            //       project.remove();
+            //     });
+            //   });
+      
+              return Client.findByIdAndRemove(args.id);
+            },
+          },
+          //ADD PROJECT
+          addProject: {
+            type: ProjectType,
+            args: {
+              name: { type: GraphQLNonNull(GraphQLString) },
+              description: { type: GraphQLNonNull(GraphQLString) },
+              status: {
+                type: new GraphQLEnumType({
+                    //name should be unique and self-explanatory 
+                  name: 'ProjectStatus',
+                  values: {
+                    new: { value: 'Not Started' },
+                    progress: { value: 'In Progress' },
+                    backLog: { value: 'BackLog' },
+                    springLog: { value: 'SprintLog' },
+                    progress: { value: 'In Progress' },
+                    review: { value: 'Under Review' },
+                    validation: { value: 'Waiting Validation' },
+                    completed: { value: 'Completed' },
+                    discarded: { value: 'Discarded' },
+                  },
+                }),
+                defaultValue: 'Not Started',
+              },
+              //clientId is mandatory as well
+              clientId: { type: GraphQLNonNull(GraphQLID) },
+            },
+            resolve(parent, args) {
+              const project = new Project({
+                name: args.name,
+                description: args.description,
+                status: args.status,
+                clientId: args.clientId,
+              });
+      
+              return project.save();
+            },
+          },
+          //DELETE PROJECT
+          deleteProject: {
+            type: ProjectType,
+            args: {
+              id: { type: GraphQLNonNull(GraphQLID) },
+            },
+            resolve(parent, args) {
+              return Project.findByIdAndRemove(args.id);
+            },
+          },
+          //UPDATE PROJECT
+          updateProject: {
+            type: ProjectType,
+            args: {
+                //only the id is mandatory for obvious reasons
+              id: { type: GraphQLNonNull(GraphQLID) },
+              name: { type: GraphQLString },
+              description: { type: GraphQLString },
+              status: {
+                type: new GraphQLEnumType({
+                    //once again, the name has to be unique
+                  name: 'ProjectStatusUpdate',
+                  values: {
+                    new: { value: 'Not Started' },
+                    progress: { value: 'In Progress' },
+                    backLog: { value: 'BackLog' },
+                    springLog: { value: 'SprintLog' },
+                    progress: { value: 'In Progress' },
+                    review: { value: 'Under Review' },
+                    validation: { value: 'Waiting Validation' },
+                    completed: { value: 'Completed' },
+                    discarded: { value: 'Discarded' },
+                  },
+                }),
+              },
+            },
+            resolve(parent, args) {
+              return Project.findByIdAndUpdate(
+                args.id,
+                {
+                  $set: {
+                    name: args.name,
+                    description: args.description,
+                    status: args.status,
+                  },
+                },
+                //if not there, it gonna creates a new project
+                { new: true }
+              );
+            },
+          },
+}});
+
+
+
+
 //To be able to use this query now we need to export it as a schema
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery, mutation
 })
